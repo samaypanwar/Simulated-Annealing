e9776ff15b78a656de64f788862a99985a3910b0
samaypanwar <69972858+samaypanwar@users.noreply.github.com>
2021-12-10 01:51:34 +0530

    Added basic cost functions
    Added basic cost function that wants to minimise the sum of all the elements in the vector

---
+++ simulated_annealing/CostFunction.py
@@ -0,0 +1,12 @@
+
+def odd_point_max(vector) -> float:
+    # count = 0
+    # for idx, el in enumerate(vector):
+
+    #     if idx % 2 == 0:
+    #         count -= 1
+
+    #     else: count += 1
+
+    return sum(vector)
+
